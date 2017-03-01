a8c626e2d05c0c063f9b0c506e0989f87b228afe
Женечка <>
2017-03-01 20:59:07 +0300

    lab2 task1

---
+++ observer/WeatherData/WeatherData.h
@@ -0,0 +1,142 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include "Observer.h"
+
+using namespace std;
+
+struct SWeatherInfo
+{
+	double temperature = 0;
+	double humidity = 0;
+	double pressure = 0;
+};
+
+class CDisplay: public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+		Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+		остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		std::cout << "Current Temp " << data.temperature << std::endl;
+		std::cout << "Current Hum " << data.humidity << std::endl;
+		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+};
+
+class CStats
+{
+public:
+	CStats(std::string const& name)
+		: m_name(name)
+	{
+	}
+	void Update(double const& value)
+	{
+		m_min = std::min(value, m_min);
+		m_max = std::max(value, m_max);
+		m_acc += value;
+		++m_countAcc;
+	}
+	void Print()
+	{
+		std::cout << "Max " << m_name << m_max << std::endl;
+		std::cout << "Min " << m_name << m_min << std::endl;
+		std::cout << "Average " << m_name << (m_acc / m_countAcc) << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+private:
+	std::string m_name;
+	double m_min = std::numeric_limits<double>::infinity();
+	double m_max = -std::numeric_limits<double>::infinity();
+	double m_acc = 0;
+	unsigned m_countAcc = 0;
+};
+
+class CStatsDisplay : public IObserver<SWeatherInfo>
+{
+public:
+	CStatsDisplay()
+		: m_temperatureStats("Temp")
+		, m_humidityStats("Hum")
+		, m_pressureStats("Pressure")
+	{
+	}
+private:
+
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+	остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		m_temperatureStats.Update(data.temperature);
+		m_humidityStats.Update(data.humidity);
+		m_pressureStats.Update(data.pressure);
+		Display();
+	}
+
+	void Display()
+	{
+		m_temperatureStats.Print();
+		m_humidityStats.Print();
+		m_pressureStats.Print();
+	}
+	CStats m_temperatureStats;
+	CStats m_humidityStats;
+	CStats m_pressureStats;
+};
+
+class CWeatherData : public CObservable<SWeatherInfo>
+{
+public:
+	// Температура в градусах Цельсия
+	double GetTemperature()const
+	{
+		return m_temperature;
+	}
+	// Относительная влажность (0...100)
+	double GetHumidity()const
+	{
+		return m_humidity;
+	}
+	// Атмосферное давление (в мм.рт.ст)
+	double GetPressure()const
+	{
+		return m_pressure;
+	}
+
+	void MeasurementsChanged()
+	{
+		NotifyObservers();
+	}
+
+	void SetMeasurements(double temp, double humidity, double pressure)
+	{
+		m_humidity = humidity;
+		m_temperature = temp;
+		m_pressure = pressure;
+
+		MeasurementsChanged();
+	}
+protected:
+	SWeatherInfo GetChangedData()const override
+	{
+		SWeatherInfo info;
+		info.temperature = GetTemperature();
+		info.humidity = GetHumidity();
+		info.pressure = GetPressure();
+		return info;
+	}
+private:
+	double m_temperature = 0.0;
+	double m_humidity = 0.0;	
+	double m_pressure = 760.0;	
+};
