55060bccd3e56941fe05728efb12227183c5c118
Eugeniya <Eugeniya@TOSHA>
2017-03-22 20:03:09 +0300

    исправлены замечания по декоратору

--- decorator/coffee/Beverages.cpp
+++ decorator/coffee/Beverages.cpp
@@ -2,35 +2,35 @@
 #include <string>
 #include "Beverages.h"
 
-static const std::map <SortOfTea, std::string> TeaMap = {
+static const std::map <SortOfTea, std::string> TEA_MAP = {
 	{ SortOfTea::Ceylon, "Ceylon" },
 	{ SortOfTea::Carcade, "Carcade" },
 	{ SortOfTea::Green, "Green" },
 	{ SortOfTea::White, "White" },
 };
 
-static const std::map <VolumeOfMilkshake, std::string> MilkshakeMap = {
+static const std::map <VolumeOfMilkshake, std::string> MILKSHAKE_MAP = {
 	{ VolumeOfMilkshake::Big, "Big" },
 	{ VolumeOfMilkshake::Middle, "Middle" },
 	{ VolumeOfMilkshake::Small, "Small" },
 };
 
-static const std::map <VolumeOfMilkshake, double> MilkshakeCostMap = {
+static const std::map <VolumeOfMilkshake, double> MILKSHAKE_COST_MAP = {
 	{ VolumeOfMilkshake::Big, 80 },
 	{ VolumeOfMilkshake::Middle, 60 },
 	{ VolumeOfMilkshake::Small, 50 },
 };
 
 CMilkshake::CMilkshake(VolumeOfMilkshake volume)
-	:CBeverage(MilkshakeMap.find(volume)->second + "Milkshake")
+	:CBeverage(MILKSHAKE_MAP.at(volume) + "Milkshake")
 	, m_volume(volume)
 {}
 
 double CMilkshake::GetCost() const
 {
-	return MilkshakeCostMap.find(m_volume)->second;
+	return MILKSHAKE_COST_MAP.at(m_volume);
 }
 
 CTea::CTea(SortOfTea sort)
-	: CBeverage(TeaMap.find(sort)->second + "Tea")
+	: CBeverage(TEA_MAP.at(sort) + "Tea")
 {}
