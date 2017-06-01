a96848900486841ab2239f587756508c0196c945
Eugeniya <Eugeniya@TOSHA>
2017-06-01 21:08:07 +0300

    factory: реализована возможность загрузки данных из файла

--- factory/factory/main.cpp
+++ factory/factory/main.cpp
@@ -12,7 +12,21 @@ int main()
 	CDesigner designer;
 	CPainter painter;
 	CClient client(designer, painter);
-	client.TurnToDesigner(std::cin);
+	std::cout << "Load from file? (y/n)" << std::endl;
+	std::string answer;
+	std::cin >> answer;
+	if (answer == "y")
+	{
+		std::cout << "Enter file name: ";
+		std::string fileName;
+		std::cin >> fileName;
+		std::ifstream iStrm(fileName);
+		client.TurnToDesigner(iStrm);
+	}
+	else 
+	{
+		client.TurnToDesigner(std::cin);
+	}	
 	client.TurnToPainter(canvas);
 
 	std::ofstream oStrm(boost::filesystem::unique_path("Image%%%%.svg").string());
